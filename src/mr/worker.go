3544407620e59022e695ee0c28ac365077ed3020
zzt96 <136901341@qq.com>
2020-09-30 22:58:38 -0400

    rpc finish

--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,10 +1,11 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
-
+import (
+	"fmt"
+	"hash/fnv"
+	"log"
+	"net/rpc"
+)
 
 //
 // Map functions return a slice of KeyValue.
@@ -24,14 +25,23 @@ func ihash(key string) int {
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
-
 	// Your worker implementation here.
 
 	// uncomment to send the Example RPC to the master.
-	// CallExample()
+	CallExample()
+
+	RegWorker()
+}
+func RegWorker() {
+	args := RegisterWorkerArgs{}
+	reply := RegisterWorkerReply{}
+
+	// send the RPC request, wait for the reply.
+	call("Master.RegisterWorker", &args, &reply)
+
+	DPrintf("Got a list of files %v\n", reply.InputFiles)
 
 }
 
